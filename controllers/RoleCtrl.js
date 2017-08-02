c04cb933637c1acf9797cfe6d5ebbf171e7163bc
EricChavez <erictics@gmail.com>
2017-08-02 17:45:37 -0500

    agregando funciones de usuario

---
+++ controllers/RoleCtrl.js
@@ -0,0 +1,18 @@
+var query = require('../query');
+var sql = require('mssql');
+
+var GetRoleList = function (req, res) {
+    var request = new sql.Request();    
+    request.execute('GetRoleList', (err, result) => {
+        if (err) {
+            console.log('Error while querying database :- ' + err);
+            res.send(err);
+        } else {
+            res.send({result:result.recordset});
+        }
+    });
+};
+
+module.exports = {
+    GetRoleList
+};
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,22 @@
 const express = require('express')
 const auth = require('../middlewares/auth')
 const api = express.Router()
+const path = require('path')
+
 var userCtrl = require('../controllers/userCtrl');
 var authCtrl = require('../controllers/authCtrl');
+var roleCtrl = require('../controllers/RoleCtrl')
 
 
 api.post('/authenticate', authCtrl.singIn);
 api.post('/singup', authCtrl.singUp);
-api.post('/email-validation',auth.isAuth, userCtrl.emailvalidation);
+api.post('/email-validation', auth.isAuth, userCtrl.emailvalidation);
 api.get('/private', auth.isAuth, authCtrl.singUp);
+api.get('/role/GetRoleList', auth.isAuth, roleCtrl.GetRoleList);
+
+api.get('/image-not-found', function (req, res) {
+    res.sendFile(path.resolve('./files/account/not-available.png'));
+});
+
 
 module.exports = api
